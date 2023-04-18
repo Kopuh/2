55fd94a8bc66010cc1712120e6e44ecf1a8d36ce
Kopuh <131147146+Kopuh@users.noreply.github.com>
2023-04-18 17:10:15 +0800

    Add files via upload

---
+++ key.c
@@ -0,0 +1,37 @@
+#include <sys.h>
+#include "key.h"
+#include "delay.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+void KEY_Init(void)
+{
+ 	GPIO_InitTypeDef GPIO_InitStructure;
+ 
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);//使能PORTA,PORTE时钟
+
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_10|GPIO_Pin_11;//KEY0-KEY2
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //设置成上拉输入
+ 	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOE2,3,4
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ onenet.c
@@ -0,0 +1,295 @@
+//单片机头文件
+#include "stm32f10x.h"
+
+//网络设备
+#include "esp8266.h"
+
+//协议文件
+#include "onenet.h"
+#include "mqttkit.h"
+
+//硬件驱动
+#include "usart.h"
+
+//C库
+#include <string.h>
+#include <stdio.h>
+#include "led.h"
+#include "string.h"
+#include "spi_driver.h"
+#include "stdio.h"
+#include "stm32f10x_spi.h"
+#include "beep.h"
+#include "adc.h"
+#include "string.h"
+#include "spi_driver.h"
+#include "stdio.h"
+#include "stm32f10x_spi.h"
+#include "oled.h"
+#include "KEY.h"
+uint8_t Card_Type1[2];  
+uint8_t Card_ID[4]; 
+uint8_t Card_KEY[6] = {0xff,0xff,0xff,0xff,0xff,0xff};    //{0x11,0x11,0x11,0x11,0x11,0x11};   //密码
+uint8_t Card_Data[16];
+uint8_t status;
+long int cardnum=0;
+char TEMP=80,FLOW=5;
+int TIME=0;
+int a=0,b=0,num=1,speed=500;
+	
+#define PROID		"591267"   //产品ID
+
+#define AUTH_INFO	"123456789"		//鉴权信息
+
+#define DEVID		"1067515483"	//设备ID
+
+extern unsigned char esp8266_buf[128];
+
+//==========================================================
+//	函数名称：	OneNet_DevLink
+//
+//	函数功能：	与onenet创建连接
+//
+//	入口参数：	无
+//
+//	返回参数：	1-成功	0-失败
+//
+//	说明：		与onenet平台建立连接
+//==========================================================
+_Bool OneNet_DevLink(void)
+{
+	
+	MQTT_PACKET_STRUCTURE mqttPacket = {NULL, 0, 0, 0};					//协议包
+
+	unsigned char *dataPtr;
+	
+	_Bool status = 1;
+	
+	printf("OneNet_DevLink\r\nPROID: %s,	AUIF: %s,	DEVID:%s\r\n", PROID, AUTH_INFO, DEVID);
+	
+	if(MQTT_PacketConnect(PROID, AUTH_INFO, DEVID, 256, 0, MQTT_QOS_LEVEL0, NULL, NULL, 0, &mqttPacket) == 0)
+	{
+		ESP8266_SendData(mqttPacket._data, mqttPacket._len);			//上传平台
+		
+		dataPtr = ESP8266_GetIPD(250);									//等待平台响应
+		if(dataPtr != NULL)
+		{
+			if(MQTT_UnPacketRecv(dataPtr) == MQTT_PKT_CONNACK)
+			{
+				switch(MQTT_UnPacketConnectAck(dataPtr))
+				{
+					case 0:printf("Tips:	连接成功\r\n");status = 0;break;
+					
+					case 1:printf("WARN:	连接失败：协议错误\r\n");break;
+					case 2:printf("WARN:	连接失败：非法的clientid\r\n");break;
+					case 3:printf("WARN:	连接失败：服务器失败\r\n");break;
+					case 4:printf("WARN:	连接失败：用户名或密码错误\r\n");break;
+					case 5:printf("WARN:	连接失败：非法链接(比如token非法)\r\n");break;
+					
+					default:printf("ERR:	连接失败：未知错误\r\n");break;
+				}
+			}
+		}
+		
+		MQTT_DeleteBuffer(&mqttPacket);								//删包
+	}
+	else
+		printf("WARN:	MQTT_PacketConnect Failed\r\n");
+	
+	return status;
+	
+}
+
+
+	
+unsigned char OneNet_FillBuf(char *buf)
+{
+	char text[32];u16 pm,gm; 
+
+pm=Get_GP2Y_Average(20);
+	delay_ms(100);
+gm=330-(Get_Adc_Average(ADC_Channel_1,10))/10;
+	delay_ms(100);
+			OLED_ShowNum(50,0,gm,3,16);
+
+			OLED_ShowNum(50,32,pm,3,16);
+
+					OLED_Refresh();
+if(gm<150) LED1=1;
+	else LED1=0;
+if(K1==0)
+{
+LED1=1;
+}
+if(K2==0)
+{
+LED1=0;
+}
+
+	memset(text, 0, sizeof(text));	
+	strcpy(buf, ",;");
+	memset(text, 0, sizeof(text));
+	sprintf(text, "LUX,%d Lux;", gm);
+	strcat(buf, text);
+	
+//	memset(text, 0, sizeof(text));
+//	sprintf(text, "HUMI,%d %;", DHT11_Data .humi_int);
+//	strcat(buf, text);
+	
+	memset(text, 0, sizeof(text));
+	sprintf(text, "PM2.5,%dppm;", pm);
+	strcat(buf, text);
+	
+//	memset(text, 0, sizeof(text));
+//	sprintf(text, "TIME,%ld;", TIME);
+	strcat(buf, text);
+	return strlen(buf);
+}
+
+//==========================================================
+//	函数名称：	OneNet_SendData
+//
+//	函数功能：	上传数据到平台
+//
+//	入口参数：	type：发送数据的格式
+//
+//	返回参数：	无
+//
+//	说明：		
+//==========================================================
+void OneNet_SendData(void)
+{
+	
+	MQTT_PACKET_STRUCTURE mqttPacket = {NULL, 0, 0, 0};												//协议包
+	
+	char buf[128];
+	
+	short body_len = 0, i = 0;
+	
+//	printf("Tips:	OneNet_SendData-MQTT\r\n");
+	
+	memset(buf, 0, sizeof(buf));
+	
+	body_len = OneNet_FillBuf(buf);																	//获取当前需要发送的数据流的总长度
+	
+	if(body_len)
+	{
+		if(MQTT_PacketSaveData(DEVID, body_len, NULL, 5, &mqttPacket) == 0)							//封包
+		{
+			for(; i < body_len; i++)
+				mqttPacket._data[mqttPacket._len++] = buf[i];
+			
+			ESP8266_SendData(mqttPacket._data, mqttPacket._len);									//上传数据到平台
+//			printf("Send %d Bytes\r\n", mqttPacket._len);
+			
+			MQTT_DeleteBuffer(&mqttPacket);															//删包
+		}
+		else
+			printf("WARN:	EDP_NewBuffer Failed\r\n");
+	}
+	
+}
+
+//==========================================================
+//	函数名称：	OneNet_RevPro
+//
+//	函数功能：	平台返回数据检测
+//
+//	入口参数：	dataPtr：平台返回的数据
+//
+//	返回参数：	无
+//
+//	说明：		
+//==========================================================
+void OneNet_RevPro(unsigned char *cmd)
+{
+	
+	MQTT_PACKET_STRUCTURE mqttPacket = {NULL, 0, 0, 0};								//协议包
+	
+	char *req_payload = NULL;
+	char *cmdid_topic = NULL;
+	
+	unsigned short req_len = 0;
+	
+	unsigned char type = 0;
+
+	short result = 0;
+
+	char *dataPtr = NULL;
+	char numBuf[10];
+	int num = 0;
+	
+	type = MQTT_UnPacketRecv(cmd);
+	switch(type)
+	{
+		case MQTT_PKT_CMD:															//命令下发
+			
+			result = MQTT_UnPacketCmd(cmd, &cmdid_topic, &req_payload, &req_len);	//解出topic和消息体
+			if(result == 0)
+			{
+				printf("cmdid: %s, req: %s, req_len: %d\r\n", cmdid_topic, req_payload, req_len);
+				
+				if(MQTT_PacketCmdResp(cmdid_topic, req_payload, &mqttPacket) == 0)	//命令回复组包
+				{
+					printf("Tips:	Send CmdResp\r\n");
+					
+					ESP8266_SendData(mqttPacket._data, mqttPacket._len);			//回复命令
+					MQTT_DeleteBuffer(&mqttPacket);									//删包
+				}
+			}
+	
+//			if(req_payload[0]=='1') LED=0;//下发数据进行对比，做出相应判断
+//			if(req_payload[0]=='2') LED=1;//
+		
+		break;
+			
+		case MQTT_PKT_PUBACK:														//发送Publish消息，平台回复的Ack
+		
+			if(MQTT_UnPacketPublishAck(cmd) == 0)
+//				printf("Tips:	MQTT Publish Send OK\r\n");
+			
+		break;
+		
+		default:
+			result = -1;
+		break;
+	}
+	
+	ESP8266_Clear();									//清空缓存
+	
+	if(result == -1)
+		return;
+	
+	dataPtr = strchr(req_payload, ':');					//搜索'}'
+
+	if(dataPtr != NULL && result != -1)					//如果找到了
+	{
+		dataPtr++;
+		
+		while(*dataPtr >= '0' && *dataPtr <= '9')		//判断是否是下发的命令控制数据
+		{
+			numBuf[num++] = *dataPtr++;
+		}
+		numBuf[num] = 0;
+		
+		num = atoi((const char *)numBuf);				//转为数值形式
+		
+		if(strstr((char *)req_payload, "onoff"))		//搜索"onoff"
+		{
+			printf("onoff = %d", num);
+	
+		}
+		else if(strstr((char *)req_payload, "switch"))		//搜索"switch"
+		{
+			printf("switch = %d", num);
+		
+		}
+	}
+
+	if(type == MQTT_PKT_CMD || type == MQTT_PKT_PUBLISH)
+	{
+		MQTT_FreeBuffer(cmdid_topic);
+		MQTT_FreeBuffer(req_payload);
+	}
+
+}
